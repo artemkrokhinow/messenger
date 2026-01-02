27554cd06d6da633e7890d075a1b93f10840700e
Agarenko <artkroh@gmail.com>
2026-01-02 20:56:56 +0100

    socket

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000'; 
+const SOCKET_URL = 'https://messenger-backend-g6ck.onrender.com/api'; 
 
 
 export const socket = io(SOCKET_URL, {
